42ff603e54fa08c4262e512ed1c4ec0ef3e7426f
Lara30 <havardnadine@yahoo.fr>
2017-10-11 16:57:38 +0200

    mercredi

--- src/NH/PlatformBundle/Controller/AdvertController.php
+++ src/NH/PlatformBundle/Controller/AdvertController.php
@@ -5,6 +5,8 @@
 namespace NH\PlatformBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+//use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Response;
 
 //le nom de notre contrôleur respecte le nom du fichier pour que l'autoload fonctionne
@@ -22,12 +24,49 @@ class AdvertController extends Controller
         return new Response($content);
     }
 
+    //cette méthode Testaction permet de rediriger vers l'accueil
+    public function testAction($id)
+    {
+        $url = $this->get('router')->generate('nh_platform_home');
+        //cela peut s'écrire sous plusieurs formes :
+
+//        return new RedirectResponse($url);
+//        return $this->redirect($url);
+        return $this->redirectToRoute('nh_platform_home');
+        //la méthode redirectToRoute prend directement en argument la route vers laquelle rediriger et non l'url
+    }
+    public function affichageAction($id, Request $request)
+    {
+        /*  {
+              //on veut avoir l'URL de l'annonce d'id 5
+              $url = $this->get('router')->generate(
+                  'nh_platform_affichage',//1er argument : le nom de la route
+                  array('id' => 5)//2nd argument : les valeurs des paramètres
+              );
+              return new Response("l'url de l'annonce d'id 5 est : ".$url);
+          }*/
+        //$url vaut "/platform/advert/5"
 
 //récupération et affichage de l'id dans l'url
-    public function affichageAction($id)
-    {
-//        /*$id vaut 10 si on appelle l'URL /platform/avert/10*/
-        return new Response("affichage de l'id : ".$id);
+//on injecte la requête dans les arguments de la méthode
+//   public function affichageAction($id, Request $request)
+//    {
+        //ainsi on a accès à la requête HTTP via $request
+
+
+//on récupère notre paramètre tag
+//request->query pour récupérer les params de l'URL passés en GET
+        $tag = $request->query->get('tag');
+        //le contrôleur lui même dispose d'un raccourci pour utilier la méthode
+        //renderResponse = Render
+        return $this->render('NHPlatformBundle:Advert:view.html.twig', array(
+            'id' => $id,
+                'tag' => $tag,
+        ));
+//  "affichage de l'annonce d'id : ".$id.", avec le tag : ".$tag
+//       );
+// $id vaut 10 si on appelle l'URL /platform/avert/10*/
+//  return new Response("affichage de l'id : ".$id);
     }
 
     //récupération avec plusieurs paramètres
